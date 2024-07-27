3abb9e7f10fc39c73a2fa2c6abbe86a585a8b634
ipipman <ipipman@163.com>
2024-07-27 21:58:19 +0800

    support insert expression sharding engine

--- src/main/java/cn/ipman/shading/engine/StandardShardingEngine.java
+++ src/main/java/cn/ipman/shading/engine/StandardShardingEngine.java
@@ -4,10 +4,17 @@ import cn.ipman.shading.config.ShardingProperties;
 import cn.ipman.shading.demo.model.User;
 import cn.ipman.shading.strategy.HashShardingStrategy;
 import cn.ipman.shading.strategy.ShadingStrategy;
+import com.alibaba.druid.sql.SQLUtils;
+import com.alibaba.druid.sql.ast.SQLExpr;
+import com.alibaba.druid.sql.ast.SQLName;
+import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.ast.expr.SQLIdentifierExpr;
+import com.alibaba.druid.sql.ast.statement.SQLInsertStatement;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -25,10 +32,10 @@ public class StandardShardingEngine implements ShardingEngine {
     final MultiValueMap<String, String> actualTableNames = new LinkedMultiValueMap<>();
 
     // 分库策略
-    final Map<String, ShadingStrategy> datasourceStrategy = new HashMap<>();
+    final Map<String, ShadingStrategy> datasourceStrategys = new HashMap<>();
 
     // 分表策略
-    final Map<String, ShadingStrategy> tableStrategy = new HashMap<>();
+    final Map<String, ShadingStrategy> tableStrategys = new HashMap<>();
 
 
     public StandardShardingEngine(ShardingProperties properties) {
@@ -41,14 +48,47 @@ public class StandardShardingEngine implements ShardingEngine {
                 actualTableNames.add(tableName, tableName);
             });
             // 分库、分表策略
-            datasourceStrategy.put(table, new HashShardingStrategy(tableProperties.getDatasourceStrategy()));
-            tableStrategy.put(table, new HashShardingStrategy(tableProperties.getTableStrategy()));
+            datasourceStrategys.put(table, new HashShardingStrategy(tableProperties.getDatasourceStrategy()));
+            tableStrategys.put(table, new HashShardingStrategy(tableProperties.getTableStrategy()));
         });
     }
 
 
     @Override
     public ShardingResult sharding(String sql, Object[] args) {
+
+        // 解析SQL
+        SQLStatement sqlStatement = SQLUtils.parseSingleMysqlStatement(sql);
+        // 如果是insert语句
+        if (sqlStatement instanceof SQLInsertStatement sqlInsertStatement) {
+            String table = sqlInsertStatement.getTableName().getSimpleName();
+            Map<String, Object> shardingColumnsMap = new HashMap<>();
+
+            // 获取参数列
+            List<SQLExpr> cols = sqlInsertStatement.getColumns();
+            for (int i = 0; i < cols.size(); i++) {
+                SQLExpr column = cols.get(i);
+                SQLIdentifierExpr columnName = (SQLIdentifierExpr) column;
+                String columnNameStr = columnName.getSimpleName();
+                shardingColumnsMap.put(columnNameStr, args[i]);
+            }
+
+            // sharding database
+            ShadingStrategy databaseStrategy = datasourceStrategys.get(table);
+            String targetDatabase = databaseStrategy.doSharding(actualDatabaseNames.get(table), table, shardingColumnsMap);
+
+            // sharding table
+            ShadingStrategy tableStrategy = tableStrategys.get(table);
+            String targetTable = tableStrategy.doSharding(actualTableNames.get(table), table, shardingColumnsMap);
+            System.out.println(" ====>>>> target db.table = " + targetDatabase + "." + targetTable);
+
+
+        } else {
+            // select/update/insert
+        }
+
+        // todo: 以下测试
+
         Object parameterObject = args[0];
         int id = 0;
         if (parameterObject instanceof User user) {
--- src/main/java/cn/ipman/shading/mybatis/ShardingMapperFactoryBean.java
+++ src/main/java/cn/ipman/shading/mybatis/ShardingMapperFactoryBean.java
@@ -3,15 +3,20 @@ package cn.ipman.shading.mybatis;
 import cn.ipman.shading.engine.ShardingContext;
 import cn.ipman.shading.engine.ShardingEngine;
 import cn.ipman.shading.engine.ShardingResult;
-import cn.ipman.shading.demo.model.User;
 import lombok.Setter;
+import lombok.SneakyThrows;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ParameterMapping;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.mapper.MapperFactoryBean;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Proxy;
+import java.util.List;
 
 /**
  * Factory bean for mapper.
@@ -46,12 +51,35 @@ public class ShardingMapperFactoryBean<T> extends MapperFactoryBean<T> {
             BoundSql boundSql = statement.getBoundSql(args);
             System.out.println(" ===> sql statement: " + boundSql);
 
-            // 选择库
-            ShardingResult result = engine.sharding(boundSql.getSql(), args);
+            // 选择库,经过sharding后重新生成SQL
+            Object[] params = getParams(boundSql, args);
+            ShardingResult result = engine.sharding(boundSql.getSql(), params);
             ShardingContext.set(result);
 
             return method.invoke(proxy, args);
         });
 
+    }
+
+    @SneakyThrows
+    private static Object[] getParams(BoundSql boundSql, Object[] args) {
+        Object[] params = args;
+        // 如果不是基础类或包装类
+        if (args.length == 1 && !ClassUtils.isPrimitiveOrWrapper(args[0].getClass())) {
+            Object arg = args[0];
+
+            List<String> cols = boundSql.getParameterMappings()
+                    .stream().map(ParameterMapping::getProperty).toList();
+            Object[] values = new Object[cols.size()];
+            for (int i = 0; i < cols.size(); i++) {
+                Field field = ReflectionUtils.findField(arg.getClass(), cols.get(i));
+                if (field == null) throw new IllegalArgumentException("can not find field " + cols.get(i));
+                field.setAccessible(true);
+                values[i] = field.get(arg);
+            }
+            params = values;
+        }
+
+        return params;
     }
 }
