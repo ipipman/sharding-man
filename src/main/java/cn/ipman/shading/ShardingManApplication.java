bd03da3c58abe122ccb1f6f192214da305e4b643
ipipman <ipipman@163.com>
2024-07-27 12:50:52 +0800

    init project

---
+++ src/main/java/cn/ipman/shading/ShardingManApplication.java
@@ -0,0 +1,13 @@
+package cn.ipman.shading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingManApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingManApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/ipman/shading/ShardingManApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ipman.shading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingManApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
